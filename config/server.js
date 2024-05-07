b8e4615238b352b5d17f1dfea819bbe0cc4c5f22
Hamid Sarani <h.sarani75@gmail.com>
2024-05-07 10:50:02 +0330

    public url default

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', `http://${env('HOST', '0.0.0.0')}:${env.int('PORT', 1337)}`),
+  url: env('PUBLIC_URL', `https://api.pish.run`),
 });
